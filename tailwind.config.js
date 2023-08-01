6a6cec2ebcd14c2471f31008e8b398a0a3d3c817
breinerbravob07 <breinerbravob07@gmail.com>
2023-08-01 15:21:02 -0500

    agregando vista sing-in a l proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,13 +10,24 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+
+    colors:{
+      ...colors,
+      
+      backgroudnLight: '#E78777', 
+      backgroudnDark: '#1E2664',
+      backgroudnDark2: '#1e26649a', 
     },
-  },
+    extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0,0.5)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      }
+    },
+
   plugins: [],
+}
 }
