a88592b194718e4564f872eceeb746cba5a7e842
joseph19993d <rodelosuaresj@gmail.com>
2023-08-01 21:35:36 -0500

    chages with Eslint

--- src/component/inputText/index.js
+++ src/component/inputText/index.js
@@ -1,46 +1,45 @@
-import classNames from "classnames"
-import { useState, useCallback } from "react"
-import { Eye, EyeSlash, WarningCircle } from "@phosphor-icons/react"
-import colors from "tailwindcss/colors"
-export default function InputText({ className, errorMessage, type = 'text', ...props }) {
-    const [showPassword, setShowPassword] = useState(false)
-    const [mouseOver, setMouseOver] = useState(false)
-    const isPassword = type === 'password'
-    const currentType = isPassword ? (showPassword ? 'text' : 'password') : type
-    const handleShowPassword = useCallback(() => setShowPassword(state => !state), [])
-    const handleMouseOver = useCallback(() => setMouseOver(state => !state), [])
-    return (
+import classNames from 'classnames'
+import { useState, useCallback } from 'react'
+import { Eye, EyeSlash, WarningCircle } from '@phosphor-icons/react'
+import colors from 'tailwindcss/colors'
+export default function InputText ({ className, errorMessage, type = 'text', ...props }) {
+  const [showPassword, setShowPassword] = useState(false)
+  const [mouseOver, setMouseOver] = useState(false)
+  const isPassword = type === 'password'
+  const currentType = isPassword ? (showPassword ? 'text' : 'password') : type
+  const handleShowPassword = useCallback(() => setShowPassword(state => !state), [])
+  const handleMouseOver = useCallback(() => setMouseOver(state => !state), [])
+  return (
 
-        <div className="min-h-[60px]">
-            <div className="flex relative justify-center">
-                <input
-                    className={classNames('inputs !pr-[40px]', className, {
-                        'outline outline-2 outline-red-400': errorMessage !== null
-                    })}
-                    type={currentType}
-                    {...props} />
+    <div className='min-h-[60px]'>
+      <div className='flex relative justify-center'>
+        <input
+          className={classNames('inputs !pr-[40px]', className, {
+            'outline outline-2 outline-red-400': errorMessage !== null
+          })}
+          type={currentType}
+          {...props}
+        />
 
-                <div className="absolute right-3 top-3" style={{ userSelect: 'none' }}>
-                    {isPassword ? (
-                        <div>
-                            
-                            {showPassword ?
-                                <Eye onClick={handleShowPassword} size={24} color={ colors.slate[500]} ></Eye> :
-                                <EyeSlash onClick={handleShowPassword} size={24} color={colors.slate[400]}></EyeSlash>
-                            }
-                            
-                        </div>
-                        
-                    ):
-                        errorMessage && <WarningCircle size={20} color={colors.red[600]}></WarningCircle>
-                    }
-                        
-                </div>
-        
-            
-            </div>
-            
-            {errorMessage && <span className="text-sm text-sm text-red-600 ml-1">{errorMessage}</span>}
+        <div className=' absolute right-3 top-3 ' style={{ userSelect: 'none' }}>
+          {isPassword
+            ? (
+              <div>
+
+                {showPassword
+                  ? <Eye onClick={handleShowPassword} size={24} color={colors.slate[500]} />
+                  : <EyeSlash onClick={handleShowPassword} size={24} color={colors.slate[400]} />}
+
+              </div>
+
+              )
+            : errorMessage && <WarningCircle size={20} color={colors.red[600]} />}
+
         </div>
-    )
+
+      </div>
+
+      {errorMessage && <span className='text-sm text-sm text-red-600 ml-1'>{errorMessage}</span>}
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 
-
 const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
 
-    colors:{
+    colors: {
       ...colors,
-      
-      backgroudnLight: '#E78777', 
+
+      backgroudnLight: '#E78777',
       backgroudnDark: '#1E2664',
-      backgroudnDark2: '#1e26649a', 
+      backgroudnDark2: '#1e26649a'
     },
     extend: {
       dropShadow: {
         '3xl': '0 35px 35px rgba(0, 0, 0,0.5)',
         '4xl': [
-            '0 35px 35px rgba(0, 0, 0, 0.25)',
-            '0 45px 65px rgba(0, 0, 0, 0.15)'
+          '0 35px 35px rgba(0, 0, 0, 0.25)',
+          '0 45px 65px rgba(0, 0, 0, 0.15)'
         ]
       }
     },
 
-  plugins: [],
-}
+    plugins: []
+  }
 }
