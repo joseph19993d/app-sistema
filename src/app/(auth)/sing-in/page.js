8c240208cf15354ab8eafa4904a65f513b657650
breinerbravob07 <breinerbravob07@gmail.com>
2023-08-03 00:10:34 -0500

    InputText error fixed succesfully

--- src/app/(auth)/sing-in/page.js
+++ src/app/(auth)/sing-in/page.js
@@ -1,9 +1,11 @@
-'use client'
-import { useCallback } from 'react'
-import Link from 'next/link'
-import InputText from '@/component/inputText'
-import { useFormik } from 'formik'
-import { loginValidation } from '@/utils/validations'
+"use client"
+import { useCallback } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import InputText from "@/components/inputText"
+import { useFormik } from "formik"
+import { loginValidation } from "@/utils/validations"
+
 
 const initialValues = {
 
@@ -11,73 +13,74 @@ const initialValues = {
   password: ''
 }
 
-export default function SingIn () {
+export default function SingIn() {
   const onSubmit = useCallback((value, actions) => {
-    console.log('value', value, formik)
-    setTimeout(() => {
+    console.log('value',value,formik)
+    setTimeout(()=>{
       actions.setSubmitting(false)
-    }, 3000)
+    },3000)
   }, [])
 
   const formik = useFormik({
     initialValues,
-    validationSchema: loginValidation,
+    validationSchema:loginValidation,
     onSubmit
   })
+
+ 
 
   const isDisable = !formik.isValid || formik.isSubmitting && formik.dirty
 
   console.log('formik', formik.errors)
-  console.log('isDisable', formik.dirty)
+  console.log('isDisable', formik.dirty )
   return (
-    <main className='flex justify-center min-h-screen bg-white md:bg-backgroudnDark md:items-center'>
-      <div className='container flex justify-center '>
+    <main className="flex justify-center min-h-screen bg-white md:bg-backgroudnDark md:items-center">
+      <div className="container flex justify-center md:w-54 md:rounded-3xl md:items-center md:mb-20 h-fit md:drop-shadow-3xl">
+          
 
-        <div className=' flex justify-center max-w-6x1 w-full  md:rounded-3xl md:items-center md:mb-20 h-[500px] md:drop-shadow-3xl'>
-          <div className='hidden relative h-full md:rounded-2xl  bg-backgroudnDark  md:flex  md:flex-1 ' />
-          <div className='flex bg-white  flex-1 mt-40  px- h-full justify-center  md:rounded-r-2xl md:px-10 md:py-20 md:mt-0'>
-            <form className='flex flex-1 flex-col' onSubmit={formik.handleSubmit}>
+          <div className="flex bg-white  mt-40  px- h-full justify-center  md:rounded-r-2xl md:px-10 md:py-20 md:mt-0">
+            <form className="flex flex-1 flex-col" onSubmit={formik.handleSubmit}>
               <div>
-                <p className='text text-4xl font-bold text-backgroudnDark'>Log in</p>
-                <span className='text-base'>Sign in to start your sessione</span>
+                <p className="text text-4xl font-bold text-backgroudnDark">Log in</p>
+                <span className="text-base">Sign in to start your session</span>
               </div>
-              <div className='flex flex-col mt-6 gap-2'>
+              <div className="flex flex-col mt-6 gap-2">
 
                 <InputText
-                  name='email'
-                  type='email'
-                  placeholder='Email'
+                  name="email"
+                  type="email"
+                  placeholder="Email"
                   value={formik.values.email}
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
-                  errorMessage={formik.touched.email && formik.errors.email ? formik.errors.email : null}
+                  errorMessage={formik.touched.email && formik.errors.email ? formik.errors.email:null}
                 />
                 <InputText
-                  name='password'
-                  type='password'
-                  placeholder='Password'
+                  name="password"
+                  type="password"
+                  placeholder="Password"
                   value={formik.values.password}
                   onChange={formik.handleChange}
                   onBlur={formik.handleBlur}
-                  errorMessage={formik.touched.password && formik.errors.password ? formik.errors.password : null}
+                  errorMessage={formik.touched.password && formik.errors.password ? formik.errors.password:null}
                 />
 
               </div>
-              <div className='mb-4'>
+              <div className="mb-4">
                 <span>Trouble logging in?</span>
 
               </div>
-              <div className='m-4'>
-                <input className='btn bg-backgroudnDark' type='submit' value='log in' disabled={isDisable} />
+              <div className="m-4">
+                <input className="btn bg-backgroudnDark" type="submit" value='log in' disabled={isDisable}/>
 
               </div>
-              <div className='m-4 mt-1  text-center '>
-                <Link className='btn bg-backgroudnLight truncate ' href='/sing-up'>register a new account</Link>
+              <div className="m-4 mt-1  text-center ">
+                <Link className="btn bg-backgroudnLight truncate " href='/sing-up'>register a new account</Link>
               </div>
 
             </form>
           </div>
-        </div>
+        
 
       </div>
     </main>
