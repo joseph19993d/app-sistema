7d5f360b3c1f8293c1be90492a00f0d6af75ef92
breinerbravob07 <breinerbravob07@gmail.com>
2023-08-11 08:11:50 -0500

    Fix: Scroll issue in dashboard page.

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -11,24 +11,18 @@ typeof windows == !  'undefined' ? Modal.setAppElement(document.body) : null
 
 //import { SignOut, Gear, Money,   ChartBarHorizontal} from "@phosphor-icons/react";
 //import colors from "tailwindcss/colors";
-
-export default  function Layout({ children }) {
-    //const pathName = usePathname
-    //const isActive = useCallback((link)=>link===pathName,[])
-    //console.log('pathName',pathName )
-
-    return (
-        <main className="flex h-screen overflow-hidden bg-slate-100 relative">
-  <div className="absolute top-0 left-0 z-10 h-screen md:relative">
-    <Aside />
-  </div>
-  <div className="flex flex-1 flex-col">
-    <Nav />
-    <div className="flex flex-1 flex-col relative z-0">
-      {children}
-    </div>
-  </div>
-</main>
-
-    )
+export default async function Layout({ children }) {
+  return (
+    <main className="flex h-screen overflow-hidden bg-slate-100 relative">
+      <div className="absolute top-0 left-0 z-10 h-screen md:relative">
+        <Aside />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <Nav />
+        <div className="flex flex-1 flex-col relative z-0 overflow-y-auto">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
 }
